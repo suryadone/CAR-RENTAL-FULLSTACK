70519f595c1b4cee20af4d8f351ceede53ec083f
suryadone <donesurya66@gmail.com>
2025-10-31 01:52:48 +0530

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,14 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // 👈 important for Vercel routing
+  build: {
+    outDir: 'dist' // 👈 make sure this matches Vercel output directory
+  },
+  server: {
+    port: 5173
+  }
 })
